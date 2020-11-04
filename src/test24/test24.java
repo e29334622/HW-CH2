7652e8b16d870660a5fcf3508039cd51da4453d5
e29334622 <e29334622@yahoo.com.tw>
2020-11-04 22:50:38 +0800

    first

---
+++ src/test24/test24.java
@@ -0,0 +1,10 @@
+package test24;
+
+public class test24 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("第"+i+"次的迴圈");
+		}
+		System.out.println("迴圈結束");
+	}
+}
